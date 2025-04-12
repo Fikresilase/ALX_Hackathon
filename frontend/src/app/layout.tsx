8a35ddef879d4fbb441f2fba76724a624d4120b9
fit-s-u-m <nehemiahmaths@gmail.com>
2025-04-12 14:19:29 +0300

    add apollo graphql

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -11,6 +11,7 @@ const navItems = [
 	{ icon: <ListChecks size={18} />, label: "Statics/Report", path: "/Stat" },
 	{ icon: <Settings size={18} />, label: "Settings", path: "/settings" },
 ];
+import ApolloProviderWrapper from '../lib/apolloProvider';
 
 export default function RootLayout({
 	children,
@@ -21,37 +22,38 @@ export default function RootLayout({
 	const pathname = usePathname();
 
 	return (
-		<html lang="en">
-			<body className="bg-zinc-900 text-white">
-				<div className="flex min-h-screen">
-					{/* Sidebar */}
-					<aside className="w-[240px] bg-zinc-950 p-4 flex flex-col gap-8">
-						<div className="text-2xl font-bold tracking-wide px-2">
-							📘
-						</div>
-						<nav className="flex flex-col gap-2">
-							{navItems.map((item) => (
-								<Button
-									key={item.label}
-									variant="ghost"
-									className={`justify-start gap-2 ${
-										pathname === item.path
+		<ApolloProviderWrapper>
+			<html lang="en">
+				<body className="bg-zinc-900 text-white">
+					<div className="flex min-h-screen">
+						{/* Sidebar */}
+						<aside className="w-[240px] bg-zinc-950 p-4 flex flex-col gap-8">
+							<div className="text-2xl font-bold tracking-wide px-2">
+								📘
+							</div>
+							<nav className="flex flex-col gap-2">
+								{navItems.map((item) => (
+									<Button
+										key={item.label}
+										variant="ghost"
+										className={`justify-start gap-2 ${pathname === item.path
 											? "bg-zinc-700 text-white"
 											: "hover:bg-zinc-800 text-zinc-400"
-									}`}
-									onClick={() => router.push(item.path)}
-								>
-									{item.icon}
-									{item.label}
-								</Button>
-							))}
-						</nav>
-					</aside>
+											}`}
+										onClick={() => router.push(item.path)}
+									>
+										{item.icon}
+										{item.label}
+									</Button>
+								))}
+							</nav>
+						</aside>
 
-					{/* Dynamic Page Content */}
-					<main className="flex-1 bg-zinc-800 p-6">{children}</main>
-				</div>
-			</body>
-		</html>
+						{/* Dynamic Page Content */}
+						<main className="flex-1 bg-zinc-800 p-6">{children}</main>
+					</div>
+				</body>
+			</html>
+		</ApolloProviderWrapper>
 	);
 }
---
+++ frontend/src/lib/apolloProvider.tsx
@@ -0,0 +1,11 @@
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient';
+const ApolloProviderWrapper = ({ children }) => {
+  return (
+    <ApolloProvider client={client} >
+      {children}
+    </ApolloProvider>
+  );
+};
+
+export default ApolloProviderWrapper;
